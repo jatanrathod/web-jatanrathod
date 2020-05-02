b558b8fd5398e0ee92f413076a126c291d086e3b
Jatan Rathod <rathodjatan@gmail.com>
2020-05-03 05:21:25 +0530

    Added Text Animation

--- js/index.js
+++ js/index.js
@@ -1,56 +1,32 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
-
 window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+    var options = {
+        strings: [
+            "Failure is an option here. if things are not failing, you are not innovating. - Elon Musk",
+            "It matters if you just don't give up. - Stephen Hawking",
+            "This is the most simple and basic component of life: our struggles determine our successes. - The Subtle Art of Not Giving a F*ck",
+            "Design is not just what it looks like and feels like. Design is how it works. - Steve Jobs",
+            "In the middle of difficulty lies opportunity. - Albert Einstein",
+        ],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 3000,
+        smartBackspace: true,
+        cursorChar:"_",
+        loop:true
+    };
+    var typed = new Typed('#quotes', options);
+    var aboutTyped = new Typed('#about', {
+        strings: [ 
+            "> Hi, I am Jatan.", 
+            "> I am Creative.", 
+            "> I Love Design.", 
+            "> I Love to Develop." 
+        ],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 3000,
+        smartBackspace: true,
+        cursorChar:"_",
+        loop:true
+    });
 };
